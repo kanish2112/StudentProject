bf4f410e2899085001279b6270c31ecbd68dfdf0
kanish2112 <bhavskan@sheridancollege.ca>
2022-05-26 09:31:39 -0400

    version 1 of student project Hello added

--- src/studentdetails/StudentDetails.java
+++ src/studentdetails/StudentDetails.java
@@ -22,6 +22,7 @@ public class StudentDetails {
         for(int i=0;i<list.length;i++)
       {
             System.out.println(list[i].getName()+" "+list[i].getAge());
+            System.out.println("hello");
       }
         }
 
